e115093e11f2bae9abf9393efc9d03393951dfa9
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2015-04-20 19:14:00 +0800

    update for INFOCOM 2014

--- paper_list_parser.py
+++ paper_list_parser.py
@@ -1,28 +1,123 @@
+import logging
 import re
 
-class PaperParser:
-    def __init__(self):
-        self.section_regex = re.compile('#0033cc"><b>([^<]+)</b>')
+class ParserBase:
+    def output(self, content, dst):
+        open(dst, 'wb').write(content.encode('utf-8'))
+        
+    def replace_all(self, string, pattern, replacement):
+        result = re.subn(pattern, replacement, string)
+
+        return result[0]
+
+    def strip_tags(self, html):
+        pattern = '<[^>]+>'
+        return self.replace_all(html, pattern, '')
+
+    def is_section(self, line):
+        pass
+
+    def is_title(self, line):
+        pass
+
+    def is_author(self, line):
+        pass
+
+    def output_html(self, dict):
+        out = open(self.dst, 'wb')
+
+        keys = dict.keys()
+        keys.sort()
+
+        logging.info('section count: %d' % (len(keys), ))
+
+        # output header
+        header = open('header.txt', 'rb').read().decode('utf-8')
+        header = header.replace('{TITLE}', html_title)
+        out.write(header.encode('utf-8'))
+
+        # output paper list
+        for key in keys:
+            section = dict[key]
+
+            string = '<h3><span>%s</span></h3>\n<dl>\n' % (key, )
+            out.write(string.encode('utf-8'))
+
+            for paper in section:
+                title = paper[0]
+                author = paper[1]
+
+                string = '<dt>%s</dt>\n<dd>%s</dd>\n' % (title, author)
+                out.write(string.encode('utf-8'))
+        
+            string = '</dl>\n'
+            out.write(string.encode('utf-8'))
+        
+        # output footer
+        footer = open('footer.txt', 'rb').read().decode('utf-8')
+        out.write(footer.encode('utf-8'))
+
+        out.close()
+
+class ParserInfocom2014(ParserBase):
+    def __init__(self, src, dst):
+        self.section_regex = re.compile('color: #C00000; background: white">([^<]+)<o:p>')
         self.title_regex = re.compile('0in"><b>([^<]+)</b>')
         self.author_regex = re.compile('<em>(.+)</em>')
 
-        self.index = 0
+        self.src = src
+        self.dst = dst
 
-        self.dict = {}
-    
-    def strip(self, string):
-        middle = string.decode('utf8')
-        middle = middle.strip()
-        middle = middle.replace(u'\xa0', '')
+        self.html = open(src, 'rb').read().decode('utf-8')
 
-        return middle.encode('utf8')
-    
-    def parse(self, input, output):
-        pass
+    def get(self):
+        # convert to easier parse format
+        html = self.transform(self.html)
 
-    def parse_string(self, string):
-        lines = string.split('\r\n')
+        # debug purpose
+        self.output(html, 'tmp.txt')
+#         html = open('tmp.txt', 'rb').read()
 
+        # get section, title, author
+        result = self.parse(html)
+
+        self.output_html(result)
+
+    def transform(self, html):
+        html = self.replace_all(html, '[\r\n\t]', '')
+        html = self.replace_all(html, '<p', '\n<p')
+
+        return html
+
+    def is_section(self, line):
+        pattern = 'color: #C00000;'
+        pos = line.find(pattern)
+
+        chair = line.find('#222222')
+
+        if pos >= 0 and chair == -1:
+            return self.strip_tags(line).strip()
+        return ''
+
+    def is_title(self, line):
+        pattern = '<b><i><span'
+        pos = line.find(pattern)
+
+        if pos >= 0:
+            return self.strip_tags(line).strip()
+        return ''
+
+    def is_author(self, line):
+        pattern = 'class="style4"'
+        pos = line.find(pattern)
+
+        if pos >= 0:
+            return self.strip_tags(line).strip()
+        return ''
+
+    def parse(self, html):
+        lines = html.split('\n')
+        tree = {}
         section = None
         title = None
         author = None
@@ -34,6 +129,7 @@ class PaperParser:
                     raise Exception('parse', 'Got section but title or author exists', value)
 
                 section = value
+                logging.debug('section: [%s]' % (section, ))
                 continue
 
             value = self.is_title(line)
@@ -43,147 +139,43 @@ class PaperParser:
 
                 if author:
                     raise Exception('parse', 'Got title but Author exists', value)
+                if title:
+                    raise Exception('parse', 'Got title but title exists', value)
 
                 title = value
+                logging.debug('title: [%s]' % (title, ))
                 continue
 
             value = self.is_author(line)
             if value:
                 if not section or not title:
-                    raise Exception('parse', 'author found but no title or section', value)
+                    logging.info('author found but no title or section')
+                    continue
+#                     raise Exception('parse', 'author found but no title or section', value)
                     
                 author = value
+                logging.debug('author: [%s]' % (author, ))
 
-                self.dict[section].append((title, author))
+                logging.debug('section: [%s], title [%s]' % (section, title, ))
+                if section not in tree:
+                    tree[section] = []
+                tree[section].append((title, author))
 
                 title = None
                 author = None
                 continue
+            else:
+                logging.info('not author, line: %s' % (line, ))
 
-        return self.dict
-
-    def is_section(self, line):
-        obj = self.section_regex.search(line)
-
-        if not obj:
-            return None
-        else:
-            section = obj.group(1)
-            section = self.strip(section)
-
-            self.dict[section] = []
-
-            return section
-
-    def is_title(self, line):
-        obj = self.title_regex.search(line)
-
-        if not obj:
-            return None
-        else:
-            title = obj.group(1)
-
-            return title
-
-    def is_author(self, line):
-        obj = self.author_regex.search(line)
-
-        if not obj:
-            return None
-        else:
-            author = obj.group(1)
-
-            return author
-
-    def get_section(self, string):
-        obj = self.section_regex.search(string, self.index)
-
-        if obj:
-            print obj.start(1)
-            print obj.end(1)
-
-
-def replace_all(string, pattern, replacement):
-    result = re.subn(pattern, replacement, string)
-
-    return result[0]
-
-def set_multi_line_to_one(bytes):
-    pattern = '<b><br />\r\n  '
-    replacement = '<b>'
-
-    return replace_all(bytes, pattern, replacement)
-
-def set_i_to_em(bytes):
-    pattern = 'i>'
-    replacement = 'em>'
-
-    return replace_all(bytes, pattern, replacement)
-
-def clear_br_with_newline(bytes):
-    pattern = '<br />\r\n *</b>'
-    replacement = '</b>'
-
-    return replace_all(bytes, pattern, replacement)
-
-def main(input, output, html_title):
-    f = open(input, 'rb')
-    out = open(output, 'wb')
-
-    bytes = f.read()
-
-    tmp = bytes
-    tmp = set_multi_line_to_one(tmp)
-    tmp = set_i_to_em(tmp)
-    tmp = clear_br_with_newline(tmp)
-
-    tmpout = open('tmp.txt', 'wb')
-    tmpout.write(tmp)
-    tmpout.close()
-
-    # then use this tmp to find string...
-    #
-    # find sectino first
-    parser = PaperParser()
-    dict = parser.parse_string(tmp)
-
-    keys = dict.keys()
-    keys.sort()
-
-    print('section count: %d' % (len(keys), ))
-
-    # output header
-    header = open('header.txt', 'rb').read()
-    header = header.replace('{TITLE}', html_title)
-    out.write(header)
-
-    # output paper list
-    for key in keys:
-        section = dict[key]
-
-        string = '<h3><span>%s</span></h3>\n<dl>\n' % (key, )
-        out.write(string)
-
-        for paper in section:
-            title = paper[0]
-            author = paper[1]
-
-            string = '<dt>%s</dt>\n<dd>%s</dd>\n' % (title, author)
-            out.write(string)
-    
-        string = '</dl>\n'
-        out.write(string)
-    
-    # output footer
-    footer = open('footer.txt', 'rb').read()
-    out.write(footer)
-
-    out.close()
+        return tree
 
 if __name__ == '__main__':
-    input = 'infocom2011.htm'
-    output = 'infocom2011.result.htm'
-    html_title = 'INFOCOM 2011 Paper List'
+#     logging.basicConfig(level=logging.DEBUG, filename='debug.log')
+    logging.basicConfig(level=logging.INFO)
 
-    main(input, output, html_title)
+    input = 'src/infocom2014.htm'
+    output = 'output/infocom2014.htm'
+    html_title = 'INFOCOM 2014 Paper List'
 
+    parser = ParserInfocom2014(input, output)
+    parser.get()
